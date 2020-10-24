1531b02eb9a15dcc97ae869e6ee909fd8257bf44
dannytce <dankijkov@gmail.com>
2020-10-24 19:49:54 +0200

    feat: open portfolios without rerender

--- src/components/Portfolio/index.tsx
+++ src/components/Portfolio/index.tsx
@@ -1,10 +1,8 @@
 import React, { useState } from 'react'
 import { useRouter } from 'next/router'
 import { Image } from 'react-datocms'
-import NextLink from 'next/link'
 
 import { Modal } from '~/components/Modal'
-// import Modal from '~/components/ReactModal'
 import Player from '~/components/Player'
 import { Container } from '~/components/Page/styled'
 
@@ -20,75 +18,52 @@ import {
 const getPlayerProps = (portfolios, slug) =>
   portfolios.find((item) => item.slug === slug)
 
-export const Portfolio = ({ slug, isModalOpen, portfolios }) => {
+export const Portfolio = ({ slug: incomingSlug, isModalOpen, portfolios }) => {
   const router = useRouter()
 
-  // const [isOpen, setIsOpen] = useState(Boolean(slug))
+  const [slug, setSlug] = useState(incomingSlug)
 
-  // function handleClick(event: MouseEvent) {
-  //   event.preventDefault()
-  // }
+  function handleClick(event: MouseEvent) {
+    event.preventDefault()
+
+    const href = event?.target?.getAttribute('href')
+
+    if (href) {
+      setSlug(href.replace('/portfolio/', ''))
+      window.history.pushState({}, '', href)
+    }
+  }
+
+  function handleClose() {
+    router.push('/')
+    setSlug(null)
+  }
 
   return (
     <Container>
       {slug && (
-        <Modal isOpen onRequestClose={() => router.push('/')}>
+        <Modal isOpen onRequestClose={handleClose}>
           <Player {...getPlayerProps(portfolios, slug)} />
         </Modal>
       )}
       <List>
         {portfolios?.map((portfolio) => (
           <ListItem key={portfolio.slug}>
-            <NextLink
-              href={`/portfolio/${portfolio.slug}`}
-              scroll={false}
-              passHref
-            >
-              <Link>
-                <ImageWrapper>
-                  <Image
-                    data={{
-                      ...portfolio.thumbnail.responsiveImage,
-                      alt: `Cover Image for ${portfolio.title}. ${portfolio.thumbnail.smartTags}`,
-                    }}
-                  />
-                </ImageWrapper>
-                <Title>{portfolio.title}</Title>
-                <Description>{portfolio.subtitle}</Description>
-              </Link>
-            </NextLink>
+            <Link href={`/portfolio/${portfolio.slug}`} onClick={handleClick}>
+              <ImageWrapper>
+                <Image
+                  data={{
+                    ...portfolio.thumbnail.responsiveImage,
+                    alt: `Cover Image for ${portfolio.title}. ${portfolio.thumbnail.smartTags}`,
+                  }}
+                />
+              </ImageWrapper>
+              <Title>{portfolio.title}</Title>
+              <Description>{portfolio.subtitle}</Description>
+            </Link>
           </ListItem>
         ))}
       </List>
     </Container>
   )
 }
-
-// export default compose(
-//   withRouter,
-//   withStateHandlers(
-//     ({ detailId }) => ({
-//       isModalOpen: boolean(detailId),
-//     }),
-//     {
-//       openModal: () => (event) => {
-//         event.preventDefault()
-//         const href = event.target.getAttribute('href')
-
-//         if (href) {
-//           window.history.pushState({}, null, href)
-
-//           return {
-//             isModalOpen: true,
-//             detailId: href.replace('/portfolios/', ''),
-//           }
-//         }
-//       },
-//       closeModal: (state, props) => () => {
-//         props.router.push('/')
-
-//         return { isModalOpen: false, detailId: null }
-//       },
-//     }
-//   )
-// )(Portfolio)
