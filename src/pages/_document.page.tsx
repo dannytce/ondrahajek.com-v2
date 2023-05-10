0eee32bf747f3d8306897e3634be8aecacef85f1
dannytce <dankijkov@gmail.com>
2023-05-10 12:26:22 +0200

    feat: add gtm

--- src/pages/_document.page.tsx
+++ src/pages/_document.page.tsx
@@ -102,6 +102,17 @@ export default class MyDocument extends Document {
               `,
             }}
           />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-T58QC9G');
+              `
+            }}
+          />
           {/* eslint-enable react/no-danger */}
           <script
             // eslint-disable-next-line react/no-danger
@@ -153,6 +164,9 @@ export default class MyDocument extends Document {
           <meta name="theme-color" content="#ffffff"></meta>
         </Head>
         <body>
+          <noscript>
+            <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T58QC9G" height="0" width="0" style="display:none;visibility:hidden"></iframe>
+          </noscript>
           <Main />
           <NextScript />
         </body>
